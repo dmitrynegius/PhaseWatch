8d8573f7ad84b919d6287494246596d726c52980
Дмитрий Негиус <dmitrynegius@gmail.com>
2017-02-22 21:26:10 +0200

    Добавьте файлы проекта.

---
+++ PhaseWatch/MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PhaseWatch
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePickerWakeUp = new System.Windows.Forms.DateTimePicker();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(29, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(294, 301);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(172, 364);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "00:00:00";
+            // 
+            // dateTimePickerWakeUp
+            // 
+            this.dateTimePickerWakeUp.CalendarForeColor = System.Drawing.Color.Black;
+            this.dateTimePickerWakeUp.CalendarMonthBackground = System.Drawing.SystemColors.InfoText;
+            this.dateTimePickerWakeUp.Checked = false;
+            this.dateTimePickerWakeUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePickerWakeUp.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePickerWakeUp.Location = new System.Drawing.Point(29, 372);
+            this.dateTimePickerWakeUp.Name = "dateTimePickerWakeUp";
+            this.dateTimePickerWakeUp.ShowCheckBox = true;
+            this.dateTimePickerWakeUp.ShowUpDown = true;
+            this.dateTimePickerWakeUp.Size = new System.Drawing.Size(109, 26);
+            this.dateTimePickerWakeUp.TabIndex = 2;
+            this.dateTimePickerWakeUp.ValueChanged += new System.EventHandler(this.dateTimePickerWakeUp_ValueChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(351, 412);
+            this.Controls.Add(this.dateTimePickerWakeUp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Phase Watch";
+            this.SizeChanged += new System.EventHandler(this.MainForm_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerWakeUp;
+    }
+}
+
---
+++ PhaseWatch/MainForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace PhaseWatch
+{
+    public partial class MainForm : Form
+    {
+        private bool hidden = false;
+        private bool SpeakNow = false;
+        Task task;
+        SpeechSynthesizer syn = new SpeechSynthesizer();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Load();
+            label1.Select();
+            dateTimePickerWakeUp.Checked = true;
+            task = Task.Run((System.Action)Speak);
+        }
+
+        public void Save()
+        {
+            try
+            {
+                string text = JsonConvert.SerializeObject(dateTimePickerWakeUp.Value, Formatting.Indented);
+                File.Delete("config.txt");
+                File.WriteAllText("config.txt", text);
+            }
+            catch { }
+        }
+
+
+        public void Load()
+        {
+            try
+            {
+                string text = File.ReadAllText("config.txt");
+                dateTimePickerWakeUp.Value = JsonConvert.DeserializeObject<DateTime>(text);
+            }
+            catch { }
+        }
+
+
+        private void HideToTray()
+        {
+            hidden = true;
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            notifyIcon1.Visible = true;
+        }
+
+        private void UnhideFromTray()
+        {
+            notifyIcon1.Visible = false;
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+            hidden = false;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (hidden)
+            {
+                UnhideFromTray();
+            }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (hidden)
+                UnhideFromTray();
+        }
+
+        private void MainForm_SizeChanged(object sender, EventArgs e)
+        {
+            if (!hidden)
+                HideToTray();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Math.Abs(DateTime.Now.Subtract(dateTimePickerWakeUp.Value).TotalSeconds)
+                < 60 * 10 && dateTimePickerWakeUp.Checked)
+            {
+                UnhideFromTray();
+                SpeakNow = true;
+            }
+            else SpeakNow = false;
+            label1.Text = "" + DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
+        }
+
+        public void Speak()
+        {
+            Random rnd = new Random();
+
+            while (true)
+            if (SpeakNow)
+            {
+                int r = rnd.Next(0, 9);
+                switch (r)
+                {
+                    case 0: syn.Speak("Wake up"); break;
+                    case 1: syn.Speak("Phase time"); break;
+                    case 2: syn.Speak("It's time to leave the body"); break;
+                    case 3: syn.Speak("Much sleep you"); break;
+                    case 4: syn.Speak("Stop sleeping"); break;
+                    case 5: syn.Speak("Leave the body"); break;
+                    case 6: syn.Speak("Imagine the swimming"); break;
+                    case 7: syn.Speak("Imagine rotation"); break;
+                    case 8: syn.Speak("Imagine the rubbing of hands before the eyes"); break;
+                    case 9: syn.Speak("Imagine the images before the eyey"); break;
+                }
+            }
+        }
+
+        private void dateTimePickerWakeUp_ValueChanged(object sender, EventArgs e)
+        {
+            Save();
+        }
+    }
+}
